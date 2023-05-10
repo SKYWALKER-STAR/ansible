8b3c473085c86cb8bef5b96da66b589cc3a814da
ming <ming@localhost.localdomain>
2023-05-10 04:54:04 -0400

    Tared

--- startup.sh
+++ startup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 
-#ansible-playbook install.yml --extra-var "hostaddress=127.0.0.1 prefix=/data/software/redis,prefixconfig=/data/software/redis/etc"
 ansible-playbook install.yml --extra-var "installroot=/data/software/redis"
-#ansible-playbook compile.yml --extra-var "source=/home/ming/Downloads/redis"
